9b9f89ad0de5b8d70622ed19556ec82fd026a116
Silver Xu <sxu@seek.com.au>
2019-03-18 11:13:38 +1100

    Fixing travis

--- build/service-worker.js
+++ build/service-worker.js
@@ -11,11 +11,11 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-
 importScripts(
-  "/precache-manifest.e6e65ec265f1d386265b04d69e82c309.js"
+    'https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js'
 );
+
+importScripts('/precache-manifest.e6e65ec265f1d386265b04d69e82c309.js');
 
 workbox.clientsClaim();
 
@@ -28,7 +28,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
-  
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+workbox.routing.registerNavigationRoute('/index.html', {
+    blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/]
 });
